e7c70251bc34cbbb3c8f0143b359f4ec6662b593
muhammed Febin Rafnad <muhammedfebinrafnadth@gmail.com>
2024-12-09 23:19:20 +0530

    updated

---
+++ nullsafety.dart
@@ -0,0 +1,7 @@
+void main(){
+  // int a;
+  // print(a); this will not work because when declaring we should assain values to it or use null safety.
+  //example.
+  int? a;
+  print(a);
+}
---
+++ static.dart
@@ -0,0 +1,10 @@
+class Car{
+  static int a=9;
+  void display(){
+    print(int);
+  }
+}
+void main(){
+  print(Car.a);
+
+}
