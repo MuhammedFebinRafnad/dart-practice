f52adca256112cd6067b0b62eaabc743cb065ebf
muhammed Febin Rafnad <muhammedfebinrafnadth@gmail.com>
2024-12-16 22:39:58 +0530

    updated

--- spread.dart
+++ spread.dart
@@ -1,5 +1,5 @@
 void main(){
-  List<int>numb1=[1,2,3,4,5];
-  List<int>numb2=[...numb1,6,7,8,9,10];
-  print(numb2);
+  List<int>list1=[1,2,3,4,5,6,];
+  List<int>list2=[...list1,7,8,9,10];
+  print(list2);
 }
