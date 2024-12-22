f6bd4fe48efaa207f45903e50c217053004ba3b8
muhammed Febin Rafnad <muhammedfebinrafnadth@gmail.com>
2024-12-22 21:42:48 +0530

    updated

--- mapin.dart
+++ mapin.dart
@@ -2,7 +2,8 @@ void main(){
   Map<String,int>name={
      "ben":19,
      "jhon":19,
-     "alan":12
+     "alan":12,
+     "frank":12
      
   };
 name.forEach((key,value)=>print("$key:$value"));
