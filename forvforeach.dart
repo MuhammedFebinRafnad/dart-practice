5c44a8950876938c396fa29b90bb0a10f22d3a2c
MuhammedFebinRafnad <muhammedfebinrafnadth@gmail.com>
2025-02-16 22:37:08 +0530

    Update forvforeach.dart

--- forvforeach.dart
+++ forvforeach.dart
@@ -17,6 +17,6 @@ void main(){
 }
 //forEAch with arrow function
 // void main(){
-//  List<int>nums=[1,2,3,4,5,6,7,8];
+//  List<int>nums=[1,2,3,4,5,6,7];
 //  nums.forEach((numb)=>print(numb));
 // }
